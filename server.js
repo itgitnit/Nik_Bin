83c315f76bf4aecd772e7607d5a9bdec85170805
itgitnit <Thakurnikhil2210@gmail.com>
2021-09-04 21:12:36 -0700

    deploy nikBin

---
+++ server.js
@@ -0,0 +1,56 @@
+const express = require("express")
+const app = express()
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+
+const Document = require("./models/Document")
+const mongoose = require('mongoose')
+mongoose.connect("mongodb://localhost/nik_bin", {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+})
+
+app.get('/', (req, res) => {
+
+    const code = `Welcome TO Nik_Bin!!
+    
+Use the commands in the top right corner to create a new file to share with others.`
+    res.render("code-display", { code, language: 'plaintext' })
+})
+app.get('/new', (req, res) => {
+    res.render("new")
+})
+
+app.post('/save', async (req, res) => {
+    const value = req.body.value
+    try {
+        const document = await Document.create({ value })
+        res.redirect(`/${document.id}`)
+    } catch (e) {
+        res.render("new", { value })
+    }
+})
+
+app.get('/:id/duplicate', async (req, res) => {
+    const id = req.params.id
+    try {
+        const document = await Document.findById(id)
+        res.render('new', { value: document.value })
+    } catch (e) {
+        res.redirect(`/${id}`)
+    }
+})
+
+app.get('/:id', async (req, res) => {
+    const id = req.params.id
+    try {
+        const document = await Document.findById(id)
+
+        res.render('code-display', { code: document.value, id })
+    } catch (e) {
+        res.redirect('/')
+    }
+})
+app.listen(3000)
